c3ccc4d30dbee4873b84ed4ae73e1d277b5d0455
NoumanBunty <rn9728959@gmail.com>
2024-09-03 15:27:31 +0500

    fixed

--- src/pages/Frontend/Home/Home.js
+++ src/pages/Frontend/Home/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function HomePage() {
-  useNavigate();
+ 
   return (
     <div className="main-content">
       {/* Hero Section */}
